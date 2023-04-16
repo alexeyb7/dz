b8fd608795af8dd91ea5bf76a90dc07b17ed3bc6
alexeyb7 <alexeyb7@gmail.com>
2023-04-17 00:20:04 +0300

    ADD DZ6

---
+++ dz6/z41/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3.*/
+int[] CreateArray(int Length) //создание массива
+{
+    int[] numbers = new int[Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        numbers[i] = int.Parse(Console.ReadLine());
+    }
+    return numbers;
+}
+int ReadInt(string arg)
+{
+    Console.Write($"Введите {arg}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintArray(int[] numbers) // печать массива
+{
+    Console.Write("[");
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        Console.Write(numbers[i] + "; ");
+    }
+    Console.WriteLine(numbers[numbers.Length - 1] + "]");
+}
+
+void CalcArray(int[] numbers) //вычисление колмчества чисел больше 0
+{
+    int calc = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) calc++;
+    }
+    Console.WriteLine($"Количество чисел больше 0 равно {calc}");
+}
+
+int Length = ReadInt("количество чисел");
+
+int[] numbers = CreateArray(Length);
+
+PrintArray(numbers);
+CalcArray(numbers);
+
---
+++ dz6/z43/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+int ReadInt(string arg)
+{
+    Console.Write($"Введите {arg}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int b1 = ReadInt("число B1");
+int b2 = ReadInt("число B2");
+int k1 = ReadInt("число К1");
+int k2 = ReadInt("число К2");
+
+double x = (double)(b1 - b2) / (k2 - k1);
+double y = x;
+
+Console.WriteLine($"Координаты точки пересечения x:{x}, y:{y} ");
