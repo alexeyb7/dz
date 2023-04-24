14e21aa88681434581f66f717a2ce6c472fe0b50
alexeyb7 <alexeyb7@gmail.com>
2023-04-24 03:20:31 +0300

    ADD DZ8

---
+++ dz8/z54/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] arr = CreateArray(4, 5);
+PrintArray(arr);
+Console.WriteLine();
+CorectArray(arr);
+PrintArray(arr);
+
+int[,] CreateArray(int m, int n) // создание массива
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-5, 10);
+        }
+    }
+    return array;
+}
+
+int[,] CorectArray(int[,] array) // сортировка массива по убыванию
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                int temp = array[i, k];
+                if (array[i, k] < array[i, k + 1])
+                {
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array) // печать массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
---
+++ dz8/z56/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int[,] arr = CreateArray(4, 5);
+PrintArray(arr);
+Console.WriteLine();
+NumberRowMinSumElements(arr);
+
+int[,] CreateArray(int m, int n) // создание массива
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+// Функция вывода номера строки с наименьшей суммой элементов 
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+void PrintArray(int[,] array) // печать массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
