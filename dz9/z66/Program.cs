806510a6dfa1c6e90bff4ed41f52f33c65a1dfd6
alexeyb7 <alexeyb7@gmail.com>
2023-04-27 01:01:38 +0300

    ADD DZ9

---
+++ dz9/z66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Count(m, n));
+
+int Count(int m, int n)
+{
+    int result = n;
+    if (n > m)
+    {
+        n--;
+        return result + Count(m, n);
+    }
+    return result;
+}
---
+++ dz9/z68/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+Console.WriteLine("Введите начальное число M:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Akkrmn(m, n));
+
+int Akkrmn(int m, int n) //вычисление функции Аккермана
+{
+    if (m == 0)
+        return n + 1;
+    else
+        if ((m != 0) && (n == 0))
+        return Akkrmn(m - 1, 1);
+    else
+        return Akkrmn(m - 1, Akkrmn(m, n - 1));
+}
